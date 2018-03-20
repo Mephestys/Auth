ce5c86b8b3d55afcda04c2da1c7c718c9318068a
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-03-20 16:55:13 -0400

    Refactored to move bcrypt hashing to User Schema

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
-const bodyParser = require('body-parser');
-const express = require('express');
-const session = require('express-session');
-const bcrypt = require('bcrypt');
+const bodyParser = require("body-parser");
+const express = require("express");
+const session = require("express-session");
+const bcrypt = require("bcrypt");
 
-const User = require('./user.js');
+const User = require("./user.js");
 
 const STATUS_USER_ERROR = 422;
 const BCRYPT_COST = 11;
@@ -11,11 +11,13 @@ const BCRYPT_COST = 11;
 const server = express();
 // to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
-server.use(session({
-  secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re',
-  resave: true,
-  saveUninitialized: false,
-}));
+server.use(
+  session({
+    secret: "e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re",
+    resave: true,
+    saveUninitialized: false
+  })
+);
 
 /* Sends the given err, a string or an object, to the client. Sets the status
  * code appropriately. */
@@ -28,61 +30,57 @@ const sendUserError = (err, res) => {
   }
 };
 
-server.post('/users', (req, res) => {
+server.post("/users", (req, res) => {
   const { username, password } = req.body;
   if (!username || !password) {
-    sendUserError('Username and password required', res);
+    sendUserError("Username and password required", res);
   } else {
-    bcrypt.hash(password, BCRYPT_COST, (err, passwordHash) => {
-      const createdUser = { username, passwordHash };
-      const newUser = new User(createdUser);
-      newUser.save()
-      .then(savedUser => res.json({ username: savedUser.username, passwordHash: savedUser.passwordHash }))
-      .catch(error => sendUserError(error, res));
-    });
+    const createdUser = { username, passwordHash: password };
+    const newUser = new User(createdUser);
+    newUser
+      .save(function(err, hash) {
+        if (err) sendUserError(err, res);
+      })
+      .then(savedUser => res.json(savedUser))
+      .catch(err => sendUserError(err, res));
   }
 });
 
-server.post('/log-in', (req, res) => {
+server.post("/log-in", (req, res) => {
   const { username, password } = req.body;
   if (!username || !password) {
-    sendUserError('Username and password required', res);
+    sendUserError("Username and password required", res);
   }
-  User.findOne({ username }).then((foundUser) => {
-    if (!foundUser) {
-      sendUserError('No user found with this username.', res);
-    } else {
-      bcrypt.compare(password, foundUser.passwordHash)
-        .then((test) => {
-          if (test) {
-            req.session.username = username;
-            req.session.isAuth = true;
-            res.json({ success: true });
-          } else {
-            sendUserError('Incorrect password.', res);
-          }
-        })
-        .catch(err => sendUserError(err, res));
-    }
-  }).catch(err => sendUserError(err, res));
+  User.findOne({ username })
+    .then(foundUser => {
+      if (!foundUser) {
+        sendUserError("No user found with the username");
+      } else {
+        let cb = function(err, isMatch) {
+          res.json({ success: isMatch });
+        };
+        foundUser.checkPassword(password, cb);
+      }
+    })
+    .catch(err => sendUserError(err, res));
 });
 
 const validateUser = (req, res, next) => {
-  if (!req.session.isAuth) sendUserError('Not logged in.', res);
+  if (!req.session.isAuth) sendUserError("Not logged in.", res);
   else {
     User.findOne({ username: req.session.username })
-      .select('username passwordHash -_id')
-      .then((user) => {
+      .select("username passwordHash -_id")
+      .then(user => {
         req.user = user;
         next();
       })
-      .catch((err) => {
+      .catch(err => {
         res.status(500).json({ error: err });
       });
   }
 };
 
-server.get('/me', validateUser, (req, res) => {
+server.get("/me", validateUser, (req, res) => {
   // Do NOT modify this route handler in any way.
   res.json(req.user);
 });
--- src/user.js
+++ src/user.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
+/* eslint-disable */
+
+const BCRYPT_COST = 11;
 // Clear out mongoose's model cache to allow --watch to work for tests:
 // https://github.com/Automattic/mongoose/issues/1251
 mongoose.models = {};
@@ -11,13 +15,31 @@ mongoose.connect('mongodb://localhost/users', { useMongoClient: true });
 const UserSchema = new mongoose.Schema({
   username: {
     type: String,
-    required: true,
     unique: true,
+    required: true,
+    lowercase: true
   },
   passwordHash: {
     type: String,
-    required: true,
-  },
+    required: true
+  }
 });
+
+UserSchema.pre('save', function(next) {
+  const user = this;
+  bcrypt.hash(this.passwordHash, BCRYPT_COST, function(error, hash) {
+    if (error) return next(error);
+    user.passwordHash = hash;
+    next();
+  });
+});
+
+UserSchema.methods.checkPassword = function(potentialPassword, cb) {
+  bcrypt.compare(potentialPassword, this.passwordHash, (err, isMatch) => {
+    if (err) return cb(err);
+    // console.log(isMatch);
+    cb(null, isMatch);
+  });
+};
 
 module.exports = mongoose.model('User', UserSchema);
