8661587b7e7352e3f6c7fd1483a54503ba79453d
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-03-20 17:50:47 -0400

    Some fixes to make tests happy; 10/11 passing

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
-const bodyParser = require("body-parser");
-const express = require("express");
-const session = require("express-session");
-const bcrypt = require("bcrypt");
-
-const User = require("./user.js");
+const bodyParser = require('body-parser');
+const express = require('express');
+const session = require('express-session');
+const bcrypt = require('bcrypt');
+/* eslint-disable */
+const User = require('./user.js');
 
 const STATUS_USER_ERROR = 422;
 const BCRYPT_COST = 11;
@@ -13,7 +13,7 @@ const server = express();
 server.use(bodyParser.json());
 server.use(
   session({
-    secret: "e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re",
+    secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re',
     resave: true,
     saveUninitialized: false
   })
@@ -30,10 +30,10 @@ const sendUserError = (err, res) => {
   }
 };
 
-server.post("/users", (req, res) => {
+server.post('/users', (req, res) => {
   const { username, password } = req.body;
   if (!username || !password) {
-    sendUserError("Username and password required", res);
+    sendUserError('Username and password required', res);
   } else {
     const createdUser = { username, passwordHash: password };
     const newUser = new User(createdUser);
@@ -46,32 +46,36 @@ server.post("/users", (req, res) => {
   }
 });
 
-server.post("/log-in", (req, res) => {
+server.post('/log-in', (req, res) => {
   const { username, password } = req.body;
   if (!username || !password) {
-    sendUserError("Username and password required", res);
+    sendUserError('Username and password required', res);
   }
-  User.findOne({ username })
+  User.findOne({ username: username.toLowerCase() })
     .then(foundUser => {
       if (!foundUser) {
-        sendUserError("No user found with the username");
+        sendUserError('No user found with the username');
       } else {
         let cb = function(err, isMatch) {
           res.json({ success: isMatch });
         };
         foundUser.checkPassword(password, cb);
+        req.session.username = username;
+        req.session.isAuth = true;
+        console.log(req.session);
       }
     })
     .catch(err => sendUserError(err, res));
 });
 
 const validateUser = (req, res, next) => {
-  if (!req.session.isAuth) sendUserError("Not logged in.", res);
+  if (!req.session.isAuth) sendUserError('Not logged in.', res);
   else {
     User.findOne({ username: req.session.username })
-      .select("username passwordHash -_id")
+      // .select('username passwordHash -_id')
       .then(user => {
         req.user = user;
+        console.log(req.user);
         next();
       })
       .catch(err => {
@@ -80,7 +84,7 @@ const validateUser = (req, res, next) => {
   }
 };
 
-server.get("/me", validateUser, (req, res) => {
+server.get('/me', validateUser, (req, res) => {
   // Do NOT modify this route handler in any way.
   res.json(req.user);
 });
